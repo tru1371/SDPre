fc9223c6814a9eebf56768841ad6e1390f76b7d7
Alejandro Trujillo <trujillo.alejandro1371@gmail.com>
2022-09-13 10:40:20 -0400

    Complete Objective3Lab1

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,13 @@
+public class Objective3Lab1{
+  public static void main(String[] args){
+    double num1, num2, num3, average;
+
+    num1 = 24;
+    num2 = 19;
+    num3 = 16;
+
+    average = (num1 + num2 + num3)/3;
+
+    System.out.println(average);
+  }
+}
