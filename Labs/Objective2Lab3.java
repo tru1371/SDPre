ca71fd3eeff02d7f4c38a5b0aa9fe6352803a832
Alejandro Trujillo <trujillo.alejandro1371@gmail.com>
2022-09-12 22:59:19 -0400

    Complete Objective2Lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,10 @@
+public class Objective2Lab3{
+  public static void main(String[] args){
+    int yearBorn = 1992;
+    String monthBorn = "August";
+    Boolean weekendToday = false;
+
+    System.out.println(yearBorn + "\n" + monthBorn +"\n" + weekendToday);
+
+  }
+}
