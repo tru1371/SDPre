9a11ad55f05b215f53f5a8b920fb9641b7263870
Alejandro Trujillo <trujillo.alejandro1371@gmail.com>
2022-09-12 22:41:21 -0400

    Complete Objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,11 @@
+public class Objective2Lab2{
+  public static void main(String[] args){
+    String name = "Alejandro Trujillo";
+    int age = 30;
+    String state = "California";
+
+    System.out.println("Name: " + name);
+    System.out.println("Age: " + age);
+    System.out.println("State: " + state);
+  }
+}
