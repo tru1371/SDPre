6a90eb7f9e98d72cd3a595c6b62353112c23b621
Alejandro Trujillo <trujillo.alejandro1371@gmail.com>
2022-09-17 20:46:06 -0400

    Complete Objective8Lab1

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,16 @@
+public class Objective8Lab1{
+  public static void main(String[] args){
+    /*Create a file called Objective8Lab1.java in the SDPre/Labs directory.
+    Using the Starter Code below add code to the commented areas that would:
+    Write a for loop that will print the numbers 1-10.
+    Write another for loop that will print the numbers 10-1.*/
+
+    for(int i = 1; i <= 10; i++){
+      System.out.println(i);
+    }
+    for(int j = 10; j >= 1; j--){
+      System.out.println(j);
+    }
+
+  }
+}
